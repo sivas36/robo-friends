219768b405c90d4bd499e5d0110d57124c3d0262
sivas36 <67056444+sivas36@users.noreply.github.com>
2020-07-20 14:42:13 +0530

    Updates

---
+++ precache-manifest.1a71b78c650211c4800ee3201bd1fe25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f521e7f32875c58cacc606a9fa7c8b18",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d40539b322b143cdc6d",
+    "url": "/robo-friends/static/css/main.608f889f.chunk.css"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/robo-friends/static/js/2.f62c00ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.f62c00ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d40539b322b143cdc6d",
+    "url": "/robo-friends/static/js/main.00ea38f5.chunk.js"
+  },
+  {
+    "revision": "899593e7014aeb6aa36e",
+    "url": "/robo-friends/static/js/runtime-main.0ad5265f.js"
+  }
+]);
